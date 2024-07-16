7b923c85a9d10bd6a6619ced7c219a5128991c77
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-07-16 22:29:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityinput");
 const card = document.querySelector(".card");
 const apikey = "22dad77cd3bb260aa2a5773bb02b0e1a";
+
+weatherForm.addEventListener('subit',event => {
+
+});
+
+async function getWeatherData(city){
+
+}
+function displayWeatherInfo(data){
+
+}
+function getWeatherEmoji(weatherId){
+
+}
+function displayError(message){
+    
+}
