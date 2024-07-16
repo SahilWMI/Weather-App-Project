995de4dbd60dc7ce0965f5abb62bf7c28a246cae
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-07-16 22:27:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-const
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityinput");
+const card = document.querySelector(".card");
+const apikey = "22dad77cd3bb260aa2a5773bb02b0e1a";
